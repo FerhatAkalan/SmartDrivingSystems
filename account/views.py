ad5bda5c84cbbfada2a7b1dc555a87f1e22b2251
Betul Mumcu <betuullm01@gmail.com>
2024-04-05 18:26:03 +0300

    account-login-procedures

--- account/views.py
+++ account/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def user_login(request):
-    return render(request,"account/login.html")
+    if request.method =="POST":
+        username=request.POST["username"]
+        password=request.POST["password"]
+        user= authenticate(request, username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('upload_file')
+        else:
+            return render(request,"account/login.html",{"error":"username ya da password yanlıs"})
+
+    else:
+        return render(request,"account/login.html")
 
 def user_register(request):
     return render(request,"account/register.html")
 
 def user_logout(request):
-    return redirect("")
+    return redirect('')
