11a81a154c91355817f3f219948eca170285b1b3
Betul Mumcu <betuullm01@gmail.com>
2024-04-11 01:28:02 +0300

    database-models-without-migration

--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    userId = models.AutoField(primary_key=True)
+    userName = models.CharField(max_length=100)
+    userSurname = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    isAdmin = models.BooleanField(default=False)
--- report/models.py
+++ report/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from account.models import Users
 
-# Create your models here.
+class Driver(models.Model):
+    driverId = models.AutoField(primary_key=True)
+    userId = models.ForeignKey(Users, on_delete=models.CASCADE)
+    driverName = models.CharField(max_length=100)
+    driverSurname = models.CharField(max_length=100)
+    driverLicence = models.CharField(max_length=100)
+
+class Trips(models.Model):
+    tripId = models.AutoField(primary_key=True)
+    driverId = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    video_path = models.CharField(max_length=255)
+
+class Reports(models.Model):
+    reportId = models.AutoField(primary_key=True)
+    tripId = models.ForeignKey(Trips, on_delete=models.CASCADE)
+    report_content = models.TextField()
+    report_filename = models.CharField(max_length=255)
